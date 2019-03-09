3cae0656937d782b44e5c8fff8641fff9b8e7419
Coen van der Kamp <coen@fourdigits.nl>
2019-03-09 20:14:36 +0100

    Initial commit

---
+++ liveblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'gg6kz5d3aw%f=qma7)0#y0ugl=ygsc^wqrizenc269n5_(sddd'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

8d6243ac7b07b6612ffd1b7ac71c67bcb74c90af
Lucas Moeskops <lucas@fabrique.nl>
2019-03-16 13:01:27 +0100

    Logic for live updates of edits applied to the admin page.

--- website/models.py
+++ website/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.db.models.signals import ModelSignal
 from django.utils.timezone import now
 from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core.blocks import StreamValue, BoundBlock
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 from website.blocks import EmbedUpdate, ImageUpdate, TextUpdate
@@ -77,6 +78,31 @@ def get_block_type(update):
     return TEXT
 
 
+def sv2type(stream_value):
+    """ helper function - get type from stream value """
+    if isinstance(stream_value, BoundBlock):
+        return stream_value.block.name
+    if isinstance(stream_value, dict):
+        return stream_value['type']
+    # assume it is a list or tuple and the first item is the value
+    return stream_value[0]
+
+
+def sv2value(stream_value):
+    """ helper function - get value dict from stream value """
+    if isinstance(stream_value, BoundBlock):
+        return stream_value.value
+    if isinstance(stream_value, dict):
+        return stream_value['value']
+    # assume it is a list or tuple and the second item is the value
+    return stream_value[1]
+
+
+def mid(stream_value):
+    """ helper function - get message id for stream_value """
+    return sv2value(stream_value)['message_id']
+
+
 class LiveBlog(Page):
     body = StreamField(BLOCK_TYPES, blank=True)
     slack_channel = models.CharField(blank=True, max_length=80)
@@ -99,12 +125,15 @@ class LiveBlog(Page):
                 live_blog=self,
                 timestamp__gt=self.last_updated).order_by('-timestamp')
 
-        def mid(stream_value):
-            # get message id for stream_value
-            return stream_value.value['message_id']
-
         if self.locked:
             return
+
+        # Before saving; see what has been updated on the current page
+        updated, deleted = self.find_instance_updates()
+
+        # We don't want saving this instance to cause this update
+        # method to be called
+        self.trigger_auto_update = False
 
         # Set lock
         self.locked = True
@@ -114,8 +143,6 @@ class LiveBlog(Page):
             # Apply updates
             pending_updates = get_pending_updates()
             items = self.body.stream_data
-            updated = []
-            deleted = []
             for update in pending_updates:
                 if update.update_type == PendingUpdate.NEW_MESSAGE:
                     block_type = get_block_type(update)
@@ -125,19 +152,19 @@ class LiveBlog(Page):
                 elif update.update_type == PendingUpdate.EDIT:
                     # Find message to update. Text block updates only.
                     for option in items:
-                        if option['value']['message_id'] == update.slack_id:
+                        if mid(option) == update.slack_id:
                             item = option
                             break
                     else:
                         # No match found to edit :-(
                         continue
-                    if item['type'] == TEXT:
-                        item['value']['message'] = update.raw_update
+                    if sv2type(item) == TEXT:
+                        sv2value(item)['message'] = update.raw_update
                         updated.append(update.slack_id)
                 elif update.update_type == PendingUpdate.DELETE:
                     # Find message to delete
                     for option in items:
-                        if option['value']['message_id'] == update.slack_id:
+                        if mid(option) == update.slack_id:
                             items.remove(option)
                             deleted.append(update.slack_id)
                             break
@@ -164,11 +191,65 @@ class LiveBlog(Page):
             self.locked = False
             self.save()
 
+        # We assume this is the only method using this property
+        self.trigger_auto_update = False
+
         # Test if there were new updates while the page was locked and
         # publishing. This would still be the moment to handle them
         pending_updates = get_pending_updates()
         if pending_updates:
             self.update()
+
+    def find_instance_updates(self):
+        """ Look on the instance for updated body items """
+        updated, deleted = [], []
+
+        if not self.pk:
+            return updated, deleted
+
+        previous_version = self.__class__.objects.get(pk=self)
+        current_items = self.body.stream_data
+        previous_items = previous_version.body.stream_data
+
+        # Detect new items
+        for current_item in current_items:
+            message_id = mid(current_item)
+
+            for previous_item in previous_items:
+                if message_id == mid(previous_item):
+                    break
+            else:
+                # This item is new!
+                updated.append(message_id)
+
+        # Detect changed and deleted items
+        for previous_item in previous_items:
+            message_id = mid(previous_item)
+            for current_item in current_items:
+                if message_id == mid(current_item):
+                    if sv2type(current_item) != sv2type(previous_item) \
+                            or sv2value(current_item) != \
+                            sv2value(previous_item):
+                        updated.append(message_id)
+                    break
+            else:
+                # item is no longer there
+                deleted.append(message_id)
+
+        return updated, deleted
+
+    def save(self, *args, **kwargs):
+        if self.trigger_auto_update:
+            # This will also handle the saving
+            # Not sure if we are missing out on some parameters;
+            # we ignore `update_fields` at the moment
+            self.update()
+        else:
+            super().save(*args, **kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.trigger_auto_update = True
 
 
 class PendingUpdate(models.Model):

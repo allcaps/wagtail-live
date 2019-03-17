ed549c3c3715bd49c55b55a85ae7d27014dd65a7
Coen van der Kamp <coen@fourdigits.nl>
2019-03-17 09:43:17 +0100

    Demonstrate how to add missing emojis to the emoji package

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,8 +1,24 @@
 import emoji
 from django import template
 from django.template.defaultfilters import stringfilter
+from emoji import EMOJI_ALIAS_UNICODE
 
 register = template.Library()
+
+# Update emoji mapping.
+EMOJI_ALIAS_UNICODE.update({
+    # Smileys
+    ':zipper_mouth_face:': '🤐',
+    ':upside_down_face:': '🙃',
+    ':money_mouth_face:': '🤑',
+    ':face_with_head_bandage:': '🤕',
+    ':face_with_cowboy_hat:': '🤠',
+
+    # Gestures
+    ':spock-hand:': '🖖',
+    ':the_horns:': '🤘',
+    ':i_love_you_hand_sign:': '🤟',
+})
 
 
 @register.filter(is_safe=True)
@@ -10,6 +26,9 @@ register = template.Library()
 def emojify(val):
     """
     'Python is :thumbs_up:' => 'Python is 👍'
+
+    Unfortunately not all Slack emojis are supported.
+    You can add them yourself with `EMOJI_ALIAS_UNICODE.update`.
 
     :param val: (string)
     :return: (string)
